27a1af53daeb203f535ec50cea94843f87949c96
Trevor Campbell <trevor.d.campbell@gmail.com>
2022-12-04 00:00:24 -0800

    minor polish on viz code

--- sentry/graph.py
+++ sentry/graph.py
@@ -17,9 +17,6 @@ _NODE_COLORS = {AssetStatus.Current : "tab:green",
 class AssetGraph:
     def __init__(self):
         self.graph = nx.DiGraph()
-        self.vizgraph = None
-        self.pos = None
-        self._stale_viz = True
         self._stale_topological_sort = True
         self._cached_topological_sort = None
 
@@ -45,7 +42,6 @@ class AssetGraph:
         self.graph.add_edges_from(edges)
         new_edges = self.graph.size()
         new_nodes = self.graph.number_of_nodes()
-        self._stale_viz = True
         self._stale_topological_sort = True
         logger.info(f"Added {new_edges-old_edges} unique edges and {new_nodes-old_nodes} unique assets to the graph")
 
@@ -56,7 +52,6 @@ class AssetGraph:
         self.graph.remove_nodes_from(assets)
         new_edges = self.graph.size()
         new_nodes = self.graph.number_of_nodes()
-        self._stale_viz = True
         self._stale_topological_sort = True
         logger.info(f"Removed {old_nodes - new_nodes} assets and {old_edges - new_edges} edges from the graph")
 
@@ -169,55 +164,49 @@ class AssetGraph:
         # in order to visualize large graphs
         # collapse all subgroups within a group with all constant state 
         logger.info("Visualizing asset graph") 
-        # TODO can't cache vizgraph since structure changes whenever status changes...
-        if self._stale_viz:
-            logger.info("Asset graph structure changed; recomputing quotient graph structure and node positions")
-            groupings = {}
-            logger.info("Assembling viznodes")
-            for asset in self.graph:
-                # if the asset is part of a group,subgroup
-                if (asset.group is not None) and (asset.subgroup is not None):
-                    # collect assets in each group/subgroup
-                    if asset.group not in groupings:
-                        groupings[asset.group] = {}
-                    if asset.subgroup not in groupings[asset.group]:
-                        groupings[asset.group][asset.subgroup] = []
-                    groupings[asset.group][asset.subgroup].append(asset)
+        groupings = {}
+        logger.info("Assembling viznodes")
+        for asset in self.graph:
+            # if the asset is part of a group,subgroup
+            if (asset.group is not None) and (asset.subgroup is not None):
+                # collect assets in each group/subgroup
+                if asset.group not in groupings:
+                    groupings[asset.group] = {}
+                if asset.subgroup not in groupings[asset.group]:
+                    groupings[asset.group][asset.subgroup] = []
+                groupings[asset.group][asset.subgroup].append(asset)
+            else:
+                #ungrouped assets get their own viznodes
+                asset.viznode = ("single",asset.hash,asset.status)
+        # for grouped assets, merge into one viznode if status is the same across the subgroup
+        for group in groupings:
+            for subgroup in groupings[group]:
+                if len({asset.status for asset in groupings[group][subgroup]}) == 1:
+                    for asset in groupings[group][subgroup]:
+                        asset.viznode = ("group",group,asset.status)
                 else:
-                    #ungrouped assets get their own viznodes
-                    asset.viznode = ("single",asset.hash,asset.status)
-            # for grouped assets, merge into one viznode if status is the same across the subgroup
-            for group in groupings:
-                for subgroup in groupings[group]:
-                    if len({asset.status for asset in groupings[group][subgroup]}) == 1:
-                        for asset in groupings[group][subgroup]:
-                            asset.viznode = ("group",group,asset.status)
-                    else:
-                        for asset in groupings[group][subgroup]:
-                            asset.viznode = ("single",asset.hash,asset.status)
-            logger.info("Building vizgraph")
-            # create the vizgraph
-            self.vizgraph = nx.DiGraph()
-            # add all viznodes
-            for asset in self.graph:
-                self.vizgraph.add_edges_from([(p.viznode, asset.viznode) for p in self.graph.predecessors(asset)]) 
-            # remove any self-edges caused by viznode
-            for node in self.vizgraph:
-                try:	
-                    self.vizgraph.remove_edge(node, node)
-                except:
-                    pass
-            print([node for node in self.vizgraph])
-            logger.info(f"Quotient graph has {self.vizgraph.number_of_nodes()} nodes and {self.vizgraph.size()} edges.")
-            logger.info("Computing quotient node positions")
-            self.pos = nx.kamada_kawai_layout(self.vizgraph)
-            self.pos = nx.spring_layout(self.vizgraph, pos=self.pos)
-            self._stale_viz = False
+                    for asset in groupings[group][subgroup]:
+                        asset.viznode = ("single",asset.hash,asset.status)
+        logger.info("Building vizgraph")
+        # create the vizgraph
+        vizgraph = nx.DiGraph()
+        # add all viznodes
+        for asset in self.graph:
+            vizgraph.add_edges_from([(p.viznode, asset.viznode) for p in self.graph.predecessors(asset)]) 
+        # remove any self-edges caused by viznode
+        for node in vizgraph:
+            try:	
+                vizgraph.remove_edge(node, node)
+            except:
+                pass
+        logger.info(f"Quotient graph has {vizgraph.number_of_nodes()} nodes and {vizgraph.size()} edges.")
+        logger.info("Computing quotient node positions")
+        pos = nx.planar_layout(vizgraph)
+        pos = nx.spring_layout(vizgraph, pos=pos) #initialize spring with planar
         logger.info("Drawing the graph")
-        #node_colors = [_NODE_COLORS[asset.status] for asset in self.graph]
-        node_colors = [_NODE_COLORS[node[2]] for node in self.vizgraph]
-        node_sizes = [600 if node[0] == "group" else 100 for node in self.vizgraph]
-        nx.draw(self.vizgraph, pos=self.pos, node_color=node_colors, node_size=node_sizes)
+        node_colors = [_NODE_COLORS[node[2]] for node in vizgraph]
+        node_sizes = [600 if node[0] == "group" else 100 for node in vizgraph]
+        nx.draw(vizgraph, pos=pos, node_color=node_colors, node_size=node_sizes)
         plt.show()
 
 
