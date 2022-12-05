92926a9345c2411a2afc468596e6b73ffbdf7ebc
Trevor Campbell <trevor.d.campbell@gmail.com>
2022-12-05 10:42:37 -0800

    added cli ideas

---
+++ sentry/cli.py
@@ -0,0 +1,8 @@
+
+# TODO:
+# - pause asset (certain/all assets)
+# - clear failure (certain/all assets)
+# - update status now (certain/all assets)
+# - run build now (certain/all assets)
+# - print summary of status
+# - update schedule for an ongoing build/update task
